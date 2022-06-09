bed0e94ad38564db2d2bb11f7961fa8fdde3e716
ignaciodmeroi <ignaciomeroi.095@gmail.com>
2022-06-09 17:13:57 -0300

    Creacion de los modelos

--- Entrega1MEROI/Hospital/models.py
+++ Entrega1MEROI/Hospital/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    dni = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.apellido+" "+self.nombre+" "+str(self.dni)
+
+class Profesional(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    especialidad = models.CharField(max_length=40)
+    matricula = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.apellido+" "+self.nombre+" "+self.especialidad
+
+class Asistente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    nempleado = models.IntegerField()
+    email = models.EmailField()
+    
+    def __str__(self) -> str:
+        return self.apellido+" "+self.nombre+" "+str(self.nempleaado)
